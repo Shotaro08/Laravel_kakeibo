53ad3f0f2a4aa5192b28e8c8e2c1db01a2f9ab38
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-05-28 09:33:29 +0900

    migration

---
+++ database/migrations/2022_05_28_001057_create_main_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMainTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('main', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('year');
+            $table->integer('month');
+            $table->integer('date');
+            $table->integer('category1_id');
+            $table->integer('category2_id');
+            $table->integer('amount');
+            $table->string('description');
+            $table->integer('payment_method_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('main');
+    }
+}

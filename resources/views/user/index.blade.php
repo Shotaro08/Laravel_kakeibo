6f0c63ec49fd133c7c9889223f31b3caba61e07a
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-05-28 10:05:47 +0900

    Seeder main

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,11 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @foreach ($e_all as $e)
-                        <div>{{ $e->name }}</div>
-                        <div class="">{{ $e->created_at->diffForHumans() }}</div>
-                        <div class="mb-4">{{ $e->email }}</div>
-
+                    @foreach ($e_main_all as $e)
+                        <div>{{ $e->year }}年</div>
+                        <div>{{ $e->month }}月</div>
+                        <div>{{ $e->date }}日</div>
+                        <div>{{ number_format($e->amount) }}円</div>
+                        <div>{{ $e->description }}</div>
                     @endforeach
                 </div>
             </div>

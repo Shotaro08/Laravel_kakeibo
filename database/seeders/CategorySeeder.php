2720c402c0bf2fdee6748f77510d863c116dccab
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-06-24 17:53:32 +0900

    databases

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -32,28 +32,5 @@ class CategorySeeder extends Seeder
                 'sort_order' => 4,
             ],
         ]);
-
-        // DB::table('secondary_categories')->insert([
-        //     [
-        //         'name' => '家賃',
-        //         'sort_order' => 1,
-        //         'primary_category_id' => 1,
-        //     ],
-        //     [
-        //         'name' => 'パルシステム',
-        //         'sort_order' => 2,
-        //         'primary_category_id' => 2,
-        //     ],
-        //     [
-        //         'name' => '電車',
-        //         'sort_order' => 3,
-        //         'primary_category_id' => 3,
-        //     ],
-        //     [
-        //         'name' => 'サウナ',
-        //         'sort_order' => 4,
-        //         'primary_category_id' => 4,
-        //     ],
-        // ]);
     }
 }

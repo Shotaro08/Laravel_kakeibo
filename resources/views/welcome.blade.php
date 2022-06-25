ef0ba9901aed9a06fa9bde503960184ef655aa9b
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-06-25 11:19:09 +0900

    responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -409,27 +409,10 @@
 <body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/dashboard') }}"
-                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">新規登録</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            <div class="flex pt-8 justify-center sm:pt-0 sm:text-sm">
                 <h1>Welcome to Lakeibo</h1>
             </div>
-
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
@@ -453,32 +436,22 @@
                     </div>
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                        <div class="flex items-center">
-                            {{-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path
-                                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z">
-                                </path>
-                                <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                            </svg> --}}
-                            <div class="container">
-                                @if (Route::has('login'))
-                                    <div class="flex mx-auto item ">
-                                        @auth
-                                            <a href="{{ url('/dashboard') }}"
-                                                class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                                        @else
-                                            <a href="{{ route('login') }}"
-                                                class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
-
-                                            @if (Route::has('register'))
-                                                <a href="{{ route('register') }}"
-                                                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">新規登録</a>
-                                            @endif
-                                        @endauth
-                                    </div>
-                                @endif
-                            </div>
+                        <div class="container">
+                            @if (Route::has('login'))
+                                <div class="mx-auto item ">
+                                    @auth
+                                        <a href="{{ url('/dashboard') }}"
+                                            class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                                    @else
+                                        <a href="{{ route('login') }}"
+                                            class="text-sm text-gray-700 dark:text-gray-500 underline font-semibold">ログイン</a>
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}"
+                                                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline font-semibold">新規登録</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
                         </div>
                     </div>
 

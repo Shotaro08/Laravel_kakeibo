7b44628266be692c248eb303fb9719b1a86a2635
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-06-26 11:11:26 +0900

    responsive create, edit

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 px-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-sm rounded-lg">
                 <div class="py-6 px-1 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font relative">
                         <div class="container px-5 mx-auto">
